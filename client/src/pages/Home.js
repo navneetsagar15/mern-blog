8667c0801b2f958c36822548d566bf968322aa85
navneetsagar15 <navneetsagar15@gmail.com>
2025-08-05 01:36:04 +0530

    initisl commit

---
+++ client/src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
+            Hello, Welcome to my blog
+        </h1>
+
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellendus odit nam provident, id ad voluptas impedit reprehenderit doloribus, ea qui expedita dolorem officiis veritatis quibusdam libero assumenda! Temporibus illo officiis esse optio voluptate? Nobis officiis, temporibus quibusdam, quam suscipit quas quo consequuntur omnis debitis quod explicabo ut modi cumque?
+        </p>
+
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio repellendus odit nam provident, id ad voluptas impedit reprehenderit doloribus, ea qui expedita dolorem officiis veritatis quibusdam libero assumenda! Temporibus illo officiis esse optio voluptate? Nobis officiis, temporibus quibusdam, quam suscipit quas quo consequuntur omnis debitis quod explicabo ut modi cumque?
+        </p>
+
+        <p className='mx-auto leading-relaxed text-base mb-4'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora dolore nam odit perspiciatis obcaecati suscipit, fuga eius impedit nisi, repudiandae ex facilis quaerat unde illum nulla iste reprehenderit quo itaque velit reiciendis deleniti error modi, ut mollitia. Quam nihil quasi voluptatem cum sunt qui iste, enim repudiandae ratione perspiciatis repellendus vero reiciendis vitae? Dignissimos possimus odit fugit eum minima maiores eveniet maxime nisi sequi facere facilis ratione labore ad ab, harum esse repellendus nulla amet aspernatur soluta vitae in. Beatae ipsum explicabo iusto non quas aut? Culpa sint voluptates vero dolores quo ex mollitia deleniti autem beatae odit. Numquam, dolorum.
+        </p>
+
+    </div>
+  )
+}
+
+export default Home
